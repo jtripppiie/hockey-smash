7d81d72bf6c3fd9536e5d39a508e7391d107816d
jtripppiie <48491097+jtripppiie@users.noreply.github.com>
2026-06-29 07:24:31 -0700

    Bump gameplay layer to v0.11.0

--- js/games/hockey-smash-v0102.js
+++ js/games/hockey-smash-v0102.js
@@ -1,6 +1,6 @@
 (function () {
-  const DISPLAY_VERSION = 'Hockey Smash v0.10.3';
-  const DISPLAY_BUILD = 'Build 2026-06-29.24';
+  const DISPLAY_VERSION = 'Hockey Smash v0.11.0';
+  const DISPLAY_BUILD = 'Build 2026-06-29.25';
   const DESIGN_WIDTH = 1024;
   const DESIGN_HEIGHT = 576;
   const GROUND_Y = DESIGN_HEIGHT * 0.82;
