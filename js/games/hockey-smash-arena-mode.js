a6a4d0652267b1720fef55e26665f656d59cdb07
jtripppiie <48491097+jtripppiie@users.noreply.github.com>
2026-06-30 09:04:19 -0700

    Sync arena build badge after background lock

--- js/games/hockey-smash-arena-mode.js
+++ js/games/hockey-smash-arena-mode.js
@@ -1,6 +1,6 @@
 (function () {
-  const DISPLAY_VERSION = 'Hockey Smash v0.14.16 Arena Mode';
-  const BUILD_LABEL = 'Hockey Smash v0.14.16 · Build 2026-06-30.72';
+  const DISPLAY_VERSION = 'Hockey Smash v0.14.17 Arena Mode';
+  const BUILD_LABEL = 'Hockey Smash v0.14.17 · Build 2026-06-30.73';
   const DESIGN_WIDTH = 1024;
   const ARENA_EDGE_PADDING = 22;
   const ARENA_CENTER_X = 440;
